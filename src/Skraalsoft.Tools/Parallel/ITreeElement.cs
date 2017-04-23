2af0b0d840e3409f1db849760e071250838b8a21
tusmester <tusmester@gmail.com>
2017-04-23 15:50:55 +0200

    Add TreeWalker tool, initial version.

---
+++ src/Skraalsoft.Tools/Parallel/ITreeElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skraalsoft.Tools.Parallel
+{
+    public interface ITreeElement
+    {
+        IEnumerable<ITreeElement> Children { get; }
+        bool IsContainer { get; }
+    }
+}
---
+++ src/Skraalsoft.Tools/Parallel/TreeWalkerOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Skraalsoft.Tools.Parallel
+{
+    /// <summary>
+    /// Options for customizing the tree walker behavior.
+    /// </summary>
+    public class TreeWalkerOptions
+    {
+        /// <summary>
+        /// Determines how many concurrent operations may occur at the same time. 
+        /// Default is 5. 1 means sequential processing.
+        /// </summary>
+        public int MaxDegreeOfParallelism { get; set; }
+
+        /// <summary>
+        /// Called every time when a tree element is reached. This is the place for 
+        /// asynchronous element processing (e.g. uploading a file) and logging.
+        /// </summary>
+        public Func<ITreeElement, Task> ProcessElementAsync { get; set; }
+
+        internal static TreeWalkerOptions Default { get; } = new TreeWalkerOptions();
+
+        /// <summary>
+        /// Creates a new instance of ImportOptions for configuring the import behavior.
+        /// </summary>
+        public TreeWalkerOptions()
+        {
+            // default values
+            MaxDegreeOfParallelism = 5;
+        }
+    }
+}
